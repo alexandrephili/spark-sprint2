06fcefbf2e1c5279c99f26e26e105486bb735cb2
alexandrephili <alexandrephili@gmail.com>
2019-07-31 06:56:48 +0400

    Updates

---
+++ precache-manifest.77bd72066f2cd5b8c4a238a41cccb099.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c37eab670f90b61ad065b8ac577d6109",
+    "url": "/spark-sprint1/index.html"
+  },
+  {
+    "revision": "7d94af4ce9be9e62e004",
+    "url": "/spark-sprint1/static/css/main.46e5704c.chunk.css"
+  },
+  {
+    "revision": "d62c9c3b3d0131b8de0b",
+    "url": "/spark-sprint1/static/js/2.145fe1e7.chunk.js"
+  },
+  {
+    "revision": "7d94af4ce9be9e62e004",
+    "url": "/spark-sprint1/static/js/main.a2e04c01.chunk.js"
+  },
+  {
+    "revision": "3f84296ac4c3f95a3c6e",
+    "url": "/spark-sprint1/static/js/runtime~main.ae0dc9e8.js"
+  }
+]);
